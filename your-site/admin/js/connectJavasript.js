48cafb48937224f98db0fd4eddee20aca6a749ae
Stepan <108026663+stepan95@users.noreply.github.com>
2023-02-08 19:15:28 +0200

    Delete your-site directory

--- your-site/admin/js/connectJavasript.js
+++
@@ -1,8 +0,0 @@
-// Функція connectJavasript для підключення нових улементів в кінець body
-function connectJavasript(url) {
-	let script = document.createElement("script");
-	script.src = url;
-	script.setAttribute('type', 'text/javascript');
-	document.body.appendChild(script);
-}
-
